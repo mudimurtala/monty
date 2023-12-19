4e7081dad4d7c2a569aec768a6ad3cd5b52c5be6
mudimurtala <ibnmuhyideen95@gmail.com>
2023-12-19 17:41:59 +0100

    monty project

--- monty.h
+++ monty.h
@@ -3,4 +3,17 @@
 
 /* Structure Declarations */
 
+typedef struct stack_s
+{
+        int n;
+        struct stack_s *prev;
+        struct stack_s *next;
+} stack_t;
+
+typedef struct instruction_s
+{
+        char *opcode;
+        void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+
 #endif /* MONTY_H */
